f2bc2c2b966b289eb72776715d32d8552abe3c97
Anthony Mittaz <sync@me.com>
2010-03-17 18:59:36 +1000

    setup default empty project

---
+++ Source/Main/AppDelegate.h
@@ -0,0 +1,21 @@
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+    
+    NSManagedObjectModel *_managedObjectModel;
+    NSManagedObjectContext *_managedObjectContext;	    
+    NSPersistentStoreCoordinator *_persistentStoreCoordinator;
+
+    UIWindow *_window;
+    UINavigationController *_navigationController;
+}
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+@property (nonatomic, retain) IBOutlet UINavigationController *navigationController;
+
+- (NSString *)applicationDocumentsDirectory;
+
+@end
+
